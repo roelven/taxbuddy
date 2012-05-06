511f4d9838417bcd37751ce60206d2a8d5f594d3
Freenerd <nospam@freenerd.de>
2012-05-06 09:06:31 +0200

    photo and hashchange magic

--- src/js/default.js
+++ src/js/default.js
@@ -1,8 +1,14 @@
-forge.logging.info("Start your app here!");
+$(function(){
 
-forge.file.getImage(function(file) {
-	forge.file.imageURL(file, function(url) {
-		document.getElementById('image').setAttribute('src', url);
-	});
+  // Bind the event.
+  $(window).hashchange( function(){
+    if (location.hash == "#page2") {
+      // Alerts every time the hash changes!
+      forge.file.getImage(function(file) {
+        forge.file.imageURL(file, function(url) {
+          $("#page2 a").first().click()
+        });
+      });
+    }
+  })
 });
-
