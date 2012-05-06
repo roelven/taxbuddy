c0d6c29622dba41c345dc3879dfb515958701e47
Freenerd <nospam@freenerd.de>
2012-05-06 10:05:38 +0200

    email validation

--- src/js/default.js
+++ src/js/default.js
@@ -14,7 +14,20 @@ $(function(){
     if (location.hash == "#page6") {
       setTimeout(function() {
         $("#page6 a").first().click();
-      }, 1500);
+      }, 3500);
     };
   })
+
+  $("#page12 a.button.visible").click(function(event) {
+    event.preventDefault();
+
+    // from http://thejimgaudet.com/articles/support/web/jquery-e-mail-validation-without-a-plugin/
+    function validateEmail($email) { var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; return $email != '' && emailReg.test( $email );}
+
+    if (validateEmail($("#textinput7").val())) {
+      $("#page12 a.hidden.button").click();
+    } else {
+      alert("no valid email");
+    };
+  });
 });
