e27495297e89eedda75473652d82cf2cc9afcf73
Freenerd <nospam@freenerd.de>
2012-05-06 04:45:39 +0200

    jQuery Mobile Mock

--- src/js/default.js
+++ src/js/default.js
@@ -1,1 +1,8 @@
-forge.logging.info("Start your app here!");
+forge.logging.info("Start your app here!");
+
+forge.file.getImage(function(file) {
+	forge.file.imageURL(file, function(url) {
+		document.getElementById('image').setAttribute('src', url);
+	});
+});
+
